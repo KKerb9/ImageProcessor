c10d5809da7ef50622ebd364b1561ac385526b74
KKerb9 <lukinkd.prog@gmail.com>
2025-10-26 20:29:44 +0300

    fix bug

--- src/app.cpp
+++ src/app.cpp
@@ -24,6 +24,20 @@ void ParseForFilters(std::vector<std::unique_ptr<Filter>>& filters, char* arr[],
 }
 
 int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        std::cerr << "\033[1;33mUSAGE:\033[0m \n\tImage Processor takes <input image path>, <output image path> and filters to apply. \n\t";
+        std::cerr << "The following filters are available:\n";
+        std::cerr << "\t\t-crop <width> <height> -- Crops the image to the specified width and height. The upper left part of the image is used.\n";
+        std::cerr << "\t\t-gs -- Converts an image into shades.\n";
+        std::cerr << "\t\t-neg -- Converts an image to a negative.\n";
+        std::cerr << "\t\t-sharp -- Image sharpening.\n";
+        std::cerr << "\t\t-edge <threshold> -- Border selection. The image is converted to grayscale.";
+        std::cerr << "Pixels with a value higher than threshold are colored white, while the rest are colored black.\n";
+        std::cerr << "\t\t-blur <sigma> -- Gaussian blur with parameter sigma.\n";
+        std::cerr << "\t\t-twirl <x coord of center> <y coord of center> <radius> <angle>";
+        std::cerr << " -- Applays twirl distortion effect with center in x, y, with the corresponding radius and angle.\n";
+        return 0;
+    }
     if (argc < 3) {
         std::cerr << "\033[1;31mERROR:\033[0m Too few arguments" << std::endl;
         return -1;
